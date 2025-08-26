d6dd37dcff9e51760e3b5cc5c4c13c90264d4b53
perletero2 <208117139+perletero2@users.noreply.github.com>
2025-08-26 18:47:28 +0200

    updated install script

--- install.sh
+++ install.sh
@@ -14,19 +14,16 @@ done
 if [ ${#missing[@]} -gt 0 ]; then
     echo "Error: The following required packages are missing: ${missing[*]}"
     echo "Installing..."
-    sudo pacman -S ${missing[*]}
+    sudo pacman -Syu --needed ${missing[*]}
     exit 1
 fi
 
 # - Create folder and clone repo -
 
-mkdir -p ~/dotfiles
-cd ~/dotfiles
-
+mkdir -p ~/dotfiles && cd ~/dotfiles
 git clone https://github.com/perletero2/dotfiles.git
 
 # - Stow dotfiles (duh!) -
 
 stow .
-
 echo "Done !"
