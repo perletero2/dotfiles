9b6f5b2560898de6ab2855ab53d842d2bddf4342
perletero2 <208117139+perletero2@users.noreply.github.com>
2025-09-01 03:04:32 +0200

    updated install script

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/env bash
+#!/usr/bin/env bash
 
 set -euo pipefail
 
@@ -18,16 +18,16 @@ done
 if [ ${#missing[@]} -gt 0 ]; then
     echo "The following required packages are missing: ${missing[*]}"
     echo "Installing..."
-    sudo pacman -S --noconfirm "${missing[*]}" || { echo "Failed to install missing packages" exit 1; }
+    sudo pacman -S --noconfirm "${missing[@]}" || { echo "Failed to install missing packages"; exit 1; }
 fi
 
 # --- Check if repo is already present else retrieve it ---
 
 if [[ ! -d "$dir" ]] ; then
-  mkdir -p "$dir" || { echo "Failed to create Dotfiles directory" exit1; }
+  mkdir -p "$dir" || { echo "Failed to create Dotfiles directory"; exit 1; }
 fi
 
-cd "$dir" || { echo "Seriously ? You managed to fail a simple cd ? Shame..." exit 1; }
+cd "$dir" || { echo "Seriously ? You managed to fail a simple cd ? Shame..."; exit 1; }
 
 updateRepo() {
   repo_url=$(git config --get remote.origin.url)
@@ -38,32 +38,32 @@ updateRepo() {
 
   if [ "$(git rev-parse --is-inside-work-tree 2>/dev/null)" != "true" ]; then # Check if current dir is a git repo, if not create it and pull from source
       echo "Cloning repo..."
-      git init || { echo "Failed to initialize git repository in $dir" exit 1; }
-      git remote set-url origin "$remote_url" || { echo "Failed to set remote url for $dir" exit 1; }
-      git pull origin main || { echo "Failed to pull updates from the remote repo" exit 1; }
+      git init || { echo "Failed to initialize git repository in $dir"; exit 1; }
+      git remote set-url origin "$remote_url" || { echo "Failed to set remote url for $dir"; exit 1; }
+      git pull origin main || { echo "Failed to pull updates from the remote repo"; exit 1; }
     
     elif [ "$repo_url" == "$remote_url" ] ; then # If it is correct repo, update it.
       echo "Updating repo..."
-      git fetch --all || { echo "Failed to fetch from source" exit 1; }
-      git stash || { echo "Failed to stash existing files" exit 1; }
-      git rebase origin/main || { echo "Failed to rebase updates from the remote repo" exit 1; } 
-      git stash pop || { echo "Failed to apply stashed changes" exit 1; }
+      git fetch --all || { echo "Failed to fetch from source"; exit 1; }
+      git stash || { echo "Failed to stash existing files"; exit 1; }
+      git rebase origin/main || { echo "Failed to rebase updates from the remote repo"; exit 1; } 
+      git stash pop || { echo "Failed to apply stashed changes"; exit 1; }
   
     elif [ "$repo_url" != "$remote_url" ] ; then # If not, correct the source and update.
       echo "Correcting repo..."
-      git remote set-url origin "$remote_url" || { echo "Failed to set remote url for $dir" exit 1; }
-      git fetch --all || { echo "Failed to fetch from source" exit 1; }
-      git stash || { echo "Failed to stash existing files" exit 1; }
-      git rebase origin/main || { echo "Failed to rebase updates from the remote repo" exit 1; }
-      git stash pop || { echo "Failed to apply stashed changes" exit 1; }
+      git remote set-url origin "$remote_url" || { echo "Failed to set remote url for $dir"; exit 1; }
+      git fetch --all || { echo "Failed to fetch from source"; exit 1; }
+      git stash || { echo "Failed to stash existing files"; exit 1; }
+      git rebase origin/main || { echo "Failed to rebase updates from the remote repo"; exit 1; }
+      git stash pop || { echo "Failed to apply stashed changes"; exit 1; }
   fi
 }
 
-updateRepo || { echo "Crappy function, go back to learning you dumbass" exit 2; }
+updateRepo || { echo "Crappy function, go back to learning you dumbass"; exit 2; }
 
 # --- Stow dotfiles (duh!) ---
 
 echo "Stowing files..."
-stow . --adopt || { echo "Failed to stow dotfiles" exit 1; }
+stow . --adopt || { echo "Failed to stow dotfiles"; exit 1; }
 echo "Done !"
 
